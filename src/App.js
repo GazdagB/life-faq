627ef81e3058abd72d7e1b300dc870e93894b05b
theCodeDesigner <gazdagbal@gmail.com>
2023-10-22 17:26:11 +0200

    FAQ App Done with a few examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import faqs from "./faqs.json"
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>❤️ FAQ: About Life</h1>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
+function Accordion({ data }) {
+  return (
+    <div className="Accordion">
+      {data.map((item, index) => {
+        return (
+          <AccordionItem
+            num={index + 1}
+            key={index}
+            title={item.title}
+            text={item.text}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function AccordionItem({num, title, text}) {
+const [isOpen, setIsOpen] = useState(false)
+
+function handleToggle(){
+  setIsOpen(prev => !prev)
+}
+
+  return (
+    <div className={`item ${isOpen ? "item-open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num <=9 ? `0${num}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div  className="content-box">{text}</div>}
+    </div>
+  );
+}
 export default App;
